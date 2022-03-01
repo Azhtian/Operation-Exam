7aefc40388869c74675c6b724631ed49cf861d4e
Sondre Nordmoen <xep015@uib.no>
2022-03-01 11:06:20 +0100

    Merged Exam into master from branch Sondre

---
+++ src/main/java/inf112/skeleton/app/Exam.java
@@ -0,0 +1,26 @@
+package inf112.skeleton.app;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+
+public class Exam extends Game{
+    public SpriteBatch batch;
+    public BitmapFont font;
+	
+	@Override
+	public void create() {
+		batch = new SpriteBatch();
+		font = new BitmapFont();
+		this.setScreen(new HomeScreen(this));
+	}
+	
+	public void render() {
+		super.render();
+	}
+	
+	public void dispose() {
+		batch.dispose();
+		font.dispose();
+	}
+}
