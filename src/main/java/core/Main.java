3cd87c7064ba84f17aff1eff5e0943f4fac308b6
Stian Rykkje <sry019@uib.no>
2022-04-08 21:52:00 +0200

    refactored map names, new intro map, changed window and game dimensions

--- src/main/java/core/Main.java
+++ src/main/java/core/Main.java
@@ -9,7 +9,7 @@ public class Main {
         Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
         // Application Title and Window Size
         cfg.setTitle("It's Exam day and you are late, run!");
-        cfg.setWindowedMode(1920, 480); // scaled as 1.5x castle board size
+        cfg.setWindowedMode(1600, 800); // scaled as 2x castle board size
         new Lwjgl3Application(new ScreenManager(), cfg);
     }
 }
